9c17c14a5b3c059d72c4029d3be2ea35f21e5555
lkf8875 <3111824424@qq.com>
2021-05-12 21:52:16 +0800

    补充注释与输出

--- src/find_ops.cc
+++ src/find_ops.cc
@@ -8,7 +8,8 @@ using std::endl;
 int main()
 {
     string numbers("0123456789"), name("r2d2");
-
+    
+    //  在name中查找numbers中任何一个字符第一次出现的位置
     auto pos = name.find_first_of(numbers);
 
     if(pos != string::npos)
@@ -22,7 +23,7 @@ int main()
     }
 
     pos = 0;
-
+    
     while((pos = name.find_first_of(numbers,pos)) != string::npos)
     {
         cout << "found number at index: " << pos
@@ -39,6 +40,7 @@ int main()
          <<  "  rfind returned: " << last_pos << endl;
 
    string dept("03714p3") ;
+   // 查找dept中第一个不在number的字符
    pos = dept.find_first_not_of(numbers) ;
    cout << "first not returned: "<< pos << endl;
 
