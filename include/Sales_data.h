1ae71d583f35922cbf4359fd591b9cc69542fc89
lkf8875 <3111824424@qq.com>
2021-06-14 11:31:59 +0800

    推送14章重载运算符内容

--- include/Sales_data.h
+++ include/Sales_data.h
@@ -25,6 +25,8 @@ public:
     std::string isbn() const { return bookNo;}
     Sales_data& operator+=(const Sales_data&);
 
+	Sales_data& combine(const Sales_data&);
+
 private:
     double avg_price() const ;
     std::string bookNo;
@@ -51,7 +53,7 @@ inline bool operator!=(const Sales_data &lhs, const Sales_data &rhs)
 
 Sales_data add(const Sales_data& , const Sales_data&);
 std::ostream &print(std::ostream& , const Sales_data&);
-std::istream &read(std::istream& , Sales_data);
+std::istream &read(std::istream& , Sales_data&);
 
 Sales_data operator+(const Sales_data&, const Sales_data&);
 std::ostream &operator<<(std::ostream&, const Sales_data&);
--- lib/Sales_data.cc
+++ lib/Sales_data.cc
@@ -13,6 +13,42 @@ double Sales_data::avg_price() const{
     else
         return 0;
 }
+// member binary operator: left-hand operand is bound to the implicit this pointer
+// assumes that both objects refer to the same book
+Sales_data& Sales_data::operator+=(const Sales_data &rhs)
+{
+	units_sold += rhs.units_sold;
+	revenue += rhs.revenue;
+	return *this;
+}
+
+// assumes that both objects refer to the same book
+Sales_data 
+operator+(const Sales_data &lhs, const Sales_data &rhs)
+{
+	Sales_data sum = lhs;  // copy data members from lhs into sum
+	sum += rhs;            // add rhs into sum
+	return sum;
+}
+
+istream &operator>>(istream &is, Sales_data &item)
+{
+	double price;  // no need to initialize; we'll read into price before we use it
+	is >> item.bookNo >> item.units_sold >> price;
+	if (is)        // check that the inputs succeeded
+    	item.revenue = item.units_sold * price;
+	else
+    	item = Sales_data(); // input failed: give the object the default state
+	return is;
+}
+
+ostream &operator<<(ostream &os, const Sales_data &item)
+{
+	os << item.isbn() << " " << item.units_sold << " " 
+	   << item.revenue << " " << item.avg_price();
+	return os;
+}
+
 
 Sales_data& Sales_data::combine(const Sales_data &rhs)
 {
